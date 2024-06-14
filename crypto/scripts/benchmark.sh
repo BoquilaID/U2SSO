34f359abe161ec00a1167cff9de1d6c95c4afb9c
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-06-14 16:06:30 -0400

    fix: fix benchmark script

--- crypto/scripts/benchmark.sh
+++ crypto/scripts/benchmark.sh
@@ -3,5 +3,5 @@
 for ((i=1; i<=10; i++))
 do
     ./semaphore_cpp/semaphore input.json witness.wtns
-    ./rapidsnark/package/bin/prover ./crypto/semaphore_js.zkey ./crypto/witness.wtns ./crypto/proof.json ./crypto/public.json
+    ../rapidsnark/package/bin/prover ./semaphore_js.zkey ./witness.wtns ./proof.json ./public.json
 done
